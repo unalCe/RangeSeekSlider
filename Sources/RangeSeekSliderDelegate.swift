094c66dc4a650b3bc46d151d1490f482cdc7dffb
Ünal Çelik <uunal.celik@gmail.com>
2019-12-20 15:47:53 +0300

    Swift 4.2 update

--- Sources/RangeSeekSliderDelegate.swift
+++ Sources/RangeSeekSliderDelegate.swift
@@ -1,58 +1,152 @@
 //
-//  RangeSeekSliderDelegate.swift
+//  TapticEngine.swift
 //  RangeSeekSlider
 //
-//  Created by Keisuke Shoji on 2017/03/09.
+//  Created by Keisuke Shoji on 2017/04/09.
 //
 //
 
-import CoreGraphics
+import UIKit
 
-public protocol RangeSeekSliderDelegate: class {
+/// Generates iOS Device vibrations by UIFeedbackGenerator.
+open class TapticEngine {
 
-    /// Called when the RangeSeekSlider values are changed
-    ///
-    /// - Parameters:
-    ///   - slider: RangeSeekSlider
-    ///   - minValue: minimum value
-    ///   - maxValue: maximum value
-    func rangeSeekSlider(_ slider: RangeSeekSlider, didChange minValue: CGFloat, maxValue: CGFloat)
-
-    /// Called when the user has started interacting with the RangeSeekSlider
-    ///
-    /// - Parameter slider: RangeSeekSlider
-    func didStartTouches(in slider: RangeSeekSlider)
-
-    /// Called when the user has finished interacting with the RangeSeekSlider
-    ///
-    /// - Parameter slider: RangeSeekSlider
-    func didEndTouches(in slider: RangeSeekSlider)
-
-    /// Called when the RangeSeekSlider values are changed. A return `String?` Value is displayed on the `minLabel`.
-    ///
-    /// - Parameters:
-    ///   - slider: RangeSeekSlider
-    ///   - minValue: minimum value
-    /// - Returns: String to be replaced
-    func rangeSeekSlider(_ slider: RangeSeekSlider, stringForMinValue minValue: CGFloat) -> String?
-
-    /// Called when the RangeSeekSlider values are changed. A return `String?` Value is displayed on the `maxLabel`.
-    ///
-    /// - Parameters:
-    ///   - slider: RangeSeekSlider
-    ///   - maxValue: maximum value
-    /// - Returns: String to be replaced
-    func rangeSeekSlider(_ slider: RangeSeekSlider, stringForMaxValue: CGFloat) -> String?
-}
+    public static let impact: Impact = Impact()
+    public static let selection: Selection = Selection()
+    public static let notification: Notification = Notification()
 
 
-// MARK: - Default implementation
+    /// Wrapper of `UIImpactFeedbackGenerator`
+    open class Impact {
 
-public extension RangeSeekSliderDelegate {
+        /// Impact feedback styles
+        ///
+        /// - light: A impact feedback between small, light user interface elements.
+        /// - medium: A impact feedback between moderately sized user interface elements.
+        /// - heavy: A impact feedback between large, heavy user interface elements.
+        public enum ImpactStyle {
+            case light, medium, heavy
+        }
 
-    func rangeSeekSlider(_ slider: RangeSeekSlider, didChange minValue: CGFloat, maxValue: CGFloat) {}
-    func didStartTouches(in slider: RangeSeekSlider) {}
-    func didEndTouches(in slider: RangeSeekSlider) {}
-    func rangeSeekSlider(_ slider: RangeSeekSlider, stringForMinValue minValue: CGFloat) -> String? { return nil }
-    func rangeSeekSlider(_ slider: RangeSeekSlider, stringForMaxValue maxValue: CGFloat) -> String? { return nil }
+        private var style: ImpactStyle = .light
+        private var generator: Any? = Impact.makeGenerator(.light)
+
+        private static func makeGenerator(_ style: ImpactStyle) -> Any? {
+            guard #available(iOS 10.0, *) else { return nil }
+
+			let feedbackStyle: UIImpactFeedbackGenerator.FeedbackStyle
+            switch style {
+            case .light:
+                feedbackStyle = .light
+            case .medium:
+                feedbackStyle = .medium
+            case .heavy:
+                feedbackStyle = .heavy
+            }
+            let generator: UIImpactFeedbackGenerator = UIImpactFeedbackGenerator(style: feedbackStyle)
+            generator.prepare()
+            return generator
+        }
+
+        private func updateGeneratorIfNeeded(_ style: ImpactStyle) {
+            guard self.style != style else { return }
+            generator = Impact.makeGenerator(style)
+            self.style = style
+        }
+
+        public func feedback(_ style: ImpactStyle) {
+            guard #available(iOS 10.0, *) else { return }
+
+            updateGeneratorIfNeeded(style)
+
+            guard let generator = generator as? UIImpactFeedbackGenerator else { return }
+
+            generator.impactOccurred()
+            generator.prepare()
+        }
+
+        public func prepare(_ style: ImpactStyle) {
+            guard #available(iOS 10.0, *) else { return }
+
+            updateGeneratorIfNeeded(style)
+
+            guard let generator = generator as? UIImpactFeedbackGenerator else { return }
+
+            generator.prepare()
+        }
+    }
+
+
+    /// Wrapper of `UISelectionFeedbackGenerator`
+    open class Selection {
+
+        private var generator: Any? = {
+            guard #available(iOS 10.0, *) else { return nil }
+
+            let generator: UISelectionFeedbackGenerator = UISelectionFeedbackGenerator()
+            generator.prepare()
+            return generator
+        }()
+
+        public func feedback() {
+            guard #available(iOS 10.0, *) else { return }
+            guard let generator = generator as? UISelectionFeedbackGenerator else { return }
+
+            generator.selectionChanged()
+            generator.prepare()
+        }
+
+        public func prepare() {
+            guard #available(iOS 10.0, *) else { return }
+            guard let generator = generator as? UISelectionFeedbackGenerator else { return }
+
+            generator.prepare()
+        }
+    }
+
+
+    /// Wrapper of `UINotificationFeedbackGenerator`
+    open class Notification {
+
+        /// Notification feedback types
+        ///
+        /// - success: A notification feedback, indicating that a task has completed successfully.
+        /// - warning: A notification feedback, indicating that a task has produced a warning.
+        /// - error: A notification feedback, indicating that a task has failed.
+        public enum NotificationType {
+            case success, warning, error
+        }
+
+        private var generator: Any? = {
+            guard #available(iOS 10.0, *) else { return nil }
+
+            let generator: UINotificationFeedbackGenerator = UINotificationFeedbackGenerator()
+            generator.prepare()
+            return generator
+        }()
+
+        public func feedback(_ type: NotificationType) {
+            guard #available(iOS 10.0, *) else { return }
+            guard let generator = generator as? UINotificationFeedbackGenerator else { return }
+
+			let feedbackType: UINotificationFeedbackGenerator.FeedbackType
+            switch type {
+            case .success:
+                feedbackType = .success
+            case .warning:
+                feedbackType = .warning
+            case .error:
+                feedbackType = .error
+            }
+            generator.notificationOccurred(feedbackType)
+            generator.prepare()
+        }
+
+        public func prepare() {
+            guard #available(iOS 10.0, *) else { return }
+            guard let generator = generator as? UINotificationFeedbackGenerator else { return }
+
+            generator.prepare()
+        }
+    }
 }
